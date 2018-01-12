d0e7fdff60db51fba7fc827538a973cb9203d8b7
mf <mf@DESKTOP-JD10JTF>
2018-01-12 12:31:27 +0800

    aa

---
+++ socket.core/Client/UdpClients.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace socket.core.Client
+{
+    internal class UdpClients
+    {
+    }
+}
---
+++ socket.core/Server/UdpServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace socket.core.Server
+{
+    internal class UdpServer
+    {
+
+    }
+}
